232d1e01081a5df6bd7c09082082d897def9897c
gdany91 <you@example.comdgarofalo52@gmail.com>
2022-12-13 16:35:45 +0100

    first commit

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,24 @@
+<script >
+
+export default {
+
+    data() {
+        return {
+            title : 'la mia prima app su vite'
+        }
+    }
+}
+
+</script>
+
+<template>
+  <h1>{{title}}</h1>
+
+  
+</template>
+
+<style scoped>
+h1 {
+  background-color: rebeccapurple;
+}
+</style>
